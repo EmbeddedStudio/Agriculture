5fe98a64871be09577c6429905321f01e71678d0
embeddedstudio <embeddedstudio@163.com>
2017-12-31 16:44:00 +0800

    feat the origin conter
    1、增加了远程控制的功能
    ２、实现了数据的上传和命令的下发
    ３、命令的下发是用 strstr 函数来解析命令的
    ４、同时也加入了继电器模块``

--- irrigation/server_1.0/server_1.0.ino
+++ irrigation/server_1.0/server_1.0.ino
@@ -1,13 +1,15 @@
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
 
 char ssid[] = "s008-flying";
 char pass[] = "dianxin132";
 
 #define GPIO0 0
 #define GPIO2 2
+#define GPIO4 4
 
-#define LEDID "3115"
-#define LEDTOKEN "52e38a188"  //数据接口
+#define LEDID "3115"          //设备接口
+#define LEDTOKEN "52e38a188"  //设备密码
 
 unsigned int httpPort = 8181;
 
@@ -21,19 +23,28 @@ void setup()
 {
   Serial.begin(115200);
   delay(10);
+  pinMode(GPIO2, OUTPUT);
+  pinMode(GPIO0, OUTPUT);
+  pinMode(GPIO4, OUTPUT);
   Serial.println("Connecting to AP ...");
   // attempt to connect to WiFi network
 
+
   WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED)
+
+  while (WiFi.status() != WL_CONNECTED)//WiFi.status() ，这个函数是wifi连接状态，返回wifi链接状态
   {
     delay(500);
     Serial.print(".");
-  }
+  }//如果没有连通向串口发送.....
+
+  Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());//WiFi.localIP()返回8266获得的ip地址
-  Serial.println("success");
+  Serial.println("mac address");
+  Serial.println(WiFi.macAddress());
+
 }
 long last_time = 0;
 void loop()
@@ -56,26 +67,57 @@ void loop()
   }
   while (client.available())//，无线读取到的数据转发到到串口
   {
+    toggle(GPIO4);
     String s = client.readString();
-    Serial.print(s);
-    delay(500);
-  }
-  if (Serial.available())//串口读取到的转发到wifi，因为串口是一位一位的发送所以在这里缓存完再发送
-  {
-    size_t counti = Serial.available();
-    uint8_t sbuf[counti];
-    Serial.readBytes(sbuf, counti);
-    client.write(sbuf, counti);
-    delay(500);
-  }
-
-  if(millis() - last_time > 3000){
-    last_time=millis();
-    String s = "{\"M\":\"update\",\"ID\":\"3115\",\"V\":{\"2948\":\"29.2\"}}\n";
-    client.print(s);
+    Serial.println(s);
+    char a[100] = "";
+    for (int i = 0; i < 100; i++)
+    {
+      a[i] = s[i];
     }
+    if ( strstr(a, "offOn") != NULL )
+    {
+      toggle(GPIO2);
+    }
+    if ( strstr(a, "play") != NULL )
+    {
+      toggle(GPIO0);
+    }
+//        Serial.println(s);
+//        Serial.println(a);
+    Serial.println(s);
+    delay(500);
+  }
+  //  if (Serial.available())//串口读取到的转发到wifi，因为串口是一位一位的发送所以在这里缓存完再发送
+  //  {
+  //    size_t counti = Serial.available();
+  //    uint8_t sbuf[counti];
+  //    Serial.readBytes(sbuf, counti);
+  //    client.write(sbuf, counti);
+  //    delay(500);
+  //  }
+
+  if (millis() - last_time > 1000) {
+    last_time = millis();
+    String s = "{\"M\":\"update\",\"ID\":\"3115\",\"V\":{\"2948\":\"15.3\"}}\n";
+    client.print(s);
+    delay(1000);
+    String t = "{\"M\":\"update\",\"ID\":\"3115\",\"V\":{\"2948\":\"30.5\"}}\n";
+    client.print(t);
+  }
 }
 
+void toggle(int GPIO)
+{
+  if ( HIGH == digitalRead(GPIO) )
+  {
+    digitalWrite(GPIO, LOW);
+  }
+  else
+  {
+    digitalWrite(GPIO, HIGH);
+  }
+}
 
 
 
